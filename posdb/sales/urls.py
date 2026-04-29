4d72a8ca1c5a4d2d4db9d8d511481668fba7925b
loeumsalit-a11y <loeumsalit@gmail.com>
2026-04-29 19:34:10 +0700

    upload project 3

---
+++ posdb/sales/urls.py
@@ -0,0 +1,10 @@
+# sales/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products-list/', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('orders/', views.order_list, name='order_list'),
+]
